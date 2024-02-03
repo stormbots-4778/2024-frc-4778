c4a7f16b1a6648a24893214c0630691daa034478
Chanhassen Stormbots <7stormbots@gmail.com>
2024-02-02 19:35:01 -0600

    .

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 2024-frc-4778
 
 [Subsystem List](https://paste-bin.us/paste/65bd89e0e0b30/)
+
+Left off on DriveSubsystem.java, and MAXSwerveModule.java
--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -15,5 +19,52 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  }
+  public static final class DriveConstants {
+    public static double spdLimitFast = .25;
+    public static double turnLimitFast = .8;
+
+    public static double spdLimitSlow = .25;
+    public static double turnLimitSlow = .25;
+    
+    // Driving Parameters - Note that these are not the maximum capable speeds of
+    // the robot, rather the allowed maximum speeds
+    public static final double kMaxSpeedMetersPerSecond = 4.6;
+    public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
+
+    public static final double kDirectionSlewRate = 1.2; // radians per second
+    public static final double kMagnitudeSlewRate = 1.2; // percent per second (1 = 100%)
+    public static final double kRotationalSlewRate = 2.0; // percent per second (1 = 100%)
+    // private final Swerve swerveSubsystem = new Swerve();
+
+    // Chassis configuration
+    public static final double kTrackWidth = Units.inchesToMeters(26.5);
+    // Distance between centers of right and left wheels on robot
+    public static final double kWheelBase = Units.inchesToMeters(26.5);
+    // Distance between front and back wheels on robot
+    public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
+        new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+        new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+        new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+        new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
+
+    // Angular offsets of the modules relative to the chassis in radians
+    public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
+    public static final double kFrontRightChassisAngularOffset = 0;
+    public static final double kBackLeftChassisAngularOffset = Math.PI;
+    public static final double kBackRightChassisAngularOffset = Math.PI / 2;
+
+    // SPARK MAX CAN IDs
+    public static final int kFrontLeftDrivingCanId = 11; //
+    public static final int kRearLeftDrivingCanId = 13; //
+    public static final int kFrontRightDrivingCanId = 15; // 
+    public static final int kRearRightDrivingCanId = 17; //
+
+    public static final int kFrontLeftTurningCanId = 10; //
+    public static final int kRearLeftTurningCanId = 12; //
+    public static final int kFrontRightTurningCanId = 14; //
+    public static final int kRearRightTurningCanId = 16; //
+
+    public static final boolean kGyroReversed = false;
   }
 }
