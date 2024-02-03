ba6c21a271f7e64f054cee4b9efb51ae502cf240
Andrew Compton <730551+acompton@users.noreply.github.com>
2024-02-02 19:07:42 -0600

    Add stub subsystems

---
+++ src/main/java/frc/robot/subsystems/CameraFlipSubsystem.java
@@ -0,0 +1,9 @@
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/**
+ * CameraFlipSubsystem will drive the camera between
+ * forward looking and upward looking positions.
+ */
+public class CameraFlipSubsystem extends SubsystemBase {
+    
+}
---
+++ src/main/java/frc/robot/subsystems/CameraSubsystem.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/*
+ * The CameraSubsystem will own the camera and logic
+ * to process images, like looking for April Tags.
+ */
+public class CameraSubsystem extends SubsystemBase {
+    
+}
