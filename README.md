107002fa1ae54deb4644df1b91d5ed5647d3e14b
Chanhassen Stormbots <7stormbots@gmail.com>
2024-02-02 19:10:36 -0600

    Added subsystem

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 2024-frc-4778
+
+[Subsystem List](https://paste-bin.us/paste/65bd89e0e0b30/)
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,5 +1,5 @@
 package frc.robot.subsystems;
 
-public class AutoaimSubsystem {
+public class DriveSubsystem {
     
 }
