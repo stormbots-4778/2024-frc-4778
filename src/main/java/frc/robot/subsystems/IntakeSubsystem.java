397a4771abb2aee14f652224a8c628d064c4d4e4
Caelen Cater <64516705+caelen-cater@users.noreply.github.com>
2024-02-07 00:51:38 +0000

    Made the IntakeSubsystem

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,5 +1,34 @@
 package frc.robot.subsystems;
 
-public class IntakeSubsystem {
-    
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.XboxController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class IntakeSubsystem extends SubsystemBase {
+    private CANSparkMax intakeMotor1, intakeMotor2, intakeMotor3;
+    private XboxController controller;
+
+    public IntakeSubsystem(XboxController controller) {
+        this.controller = controller;
+        intakeMotor1 = new CANSparkMax(1, MotorType.kBrushless);  // Caelen needs to replace these with actual port numbers
+        intakeMotor2 = new CANSparkMax(2, MotorType.kBrushless);
+        intakeMotor3 = new CANSparkMax(3, MotorType.kBrushless);
+    }
+
+    public void intake() {
+        if (controller.getAButton()) {
+            intakeMotor1.set(0.5);
+            intakeMotor2.set(0.5);
+            intakeMotor3.set(0.5);
+        }
+    }
+
+    public void outtake() {
+        if (controller.getBButton()) {
+            intakeMotor1.set(-0.5);
+            intakeMotor2.set(-0.5);
+            intakeMotor3.set(-0.5);
+        }
+    }
 }
