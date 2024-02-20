3412b9f08997a42aa8830efc47b3820759d42c22
Stormbots-4778 <7stormbots@gmail.com>
2024-02-19 19:15:05 -0600

    Updated Subsystem and Constants

--- src/main/java/frc/robot/subsystems/LauncherSubsystem.java
+++ src/main/java/frc/robot/subsystems/LauncherSubsystem.java
@@ -1,5 +1,31 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
+import frc.robot.Constants.ShooterConstants;
+
 public class LauncherSubsystem {
+
+    public CANSparkMax leftShooter, rightShooter;
+
+    // May not need because notes are squishy (would be speed controller)
+    public PIDController shooterPID = new PIDController(ShooterConstants.shooterKp, ShooterConstants.shooterKi, ShooterConstants.shooterKd);
+
+    public LauncherSubsystem() {
+        leftShooter = new CANSparkMax(ShooterConstants.kleftShooterCanId, MotorType.kBrushless);
+        rightShooter = new CANSparkMax(ShooterConstants.kleftShooterCanId, MotorType.kBrushless);
+    }
+
+    public void shoot() {
+        leftShooter.set(ShooterConstants.shootSpeed);
+        rightShooter.set(ShooterConstants.shootSpeed);
+    }
+
+    public void stop() {
+        leftShooter.set(0);
+        rightShooter.set(0);
+    }
     
 }
