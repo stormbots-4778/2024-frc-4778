417bd3bb94917fde1ae3546816d6138a140a0b21
Caelen <64516705+caelen-cater@users.noreply.github.com>
2024-02-24 16:23:53 -0600

    Created the LiftSubsystem

---
+++ src/main/java/frc/robot/subsystems/LiftSubsystem.java
@@ -0,0 +1,28 @@
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID;
+
+
+public class LiftSubsystem extends SubsystemBase {
+    private final CANSparkMax liftMotor;
+    private final XboxController controller;
+
+    public LiftSubsystem(XboxController controller) {
+        this.controller = controller;
+        liftMotor = new CANSparkMax(25, MotorType.kBrushless);
+    }
+
+    @Override
+    public void periodic() {
+        int dpadAngle = controller.getPOV();
+        if (dpadAngle == 0) { // dpad up
+            liftMotor.set(1.0);
+        } else if (dpadAngle == 180) { // dpad down
+            liftMotor.set(-1.0);
+        } else {
+            liftMotor.set(0.0);
+        }
+    }
+}
