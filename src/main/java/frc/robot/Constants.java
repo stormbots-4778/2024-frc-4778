484102f340e669a1ea698a029703ca29759e6798
Chanhassen Stormbots <7stormbots@gmail.com>
2024-03-14 20:12:30 -0500

    2024-03-14

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -50,9 +50,9 @@ public final class Constants {
     // private final Swerve swerveSubsystem = new Swerve();
 
     // Chassis configuration
-    public static final double kTrackWidth = Units.inchesToMeters(20.75);
+    public static final double kTrackWidth = Units.inchesToMeters(20.5);
     // Distance between centers of right and left wheels on robot
-    public static final double kWheelBase = Units.inchesToMeters(20.75);
+    public static final double kWheelBase = Units.inchesToMeters(20.5);
     // Distance between front and back wheels on robot
     public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
         new Translation2d(kWheelBase / 2, kTrackWidth / 2),
@@ -85,7 +85,7 @@ public final class Constants {
     public static final HolonomicPathFollowerConfig pathFollowerConfig =
       new HolonomicPathFollowerConfig(
         new PIDConstants(4, 0, 0), // TODO: Tune translation PID config
-        new PIDConstants(1, 0, 0), // TODO: Tune rotation PID config
+        new PIDConstants(0.75, 0, 0), // TODO: Tune rotation PID config
         kMaxSpeedMetersPerSecond,
         kDriveBaseRadius,
         new ReplanningConfig()
